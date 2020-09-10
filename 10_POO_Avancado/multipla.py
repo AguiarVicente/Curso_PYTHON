bac72f861011ba10ed5271a4deb71409d1a5ce8f
AguiarVicente <aguiar14564@gmail.com>
2020-09-09 22:02:19 -0300

    Programação orientada - (Avançado)

---
+++ 10_POO_Avancado/multipla.py
@@ -0,0 +1,35 @@
+# !/usr/local/bin/python3
+class Animal:
+    @property
+    def capacidade(self):
+        return ('dormir', 'comer', 'beber')
+
+
+class Homem(Animal):
+    @property
+    def capacidade(self):
+        return super().capacidade + ('amar', 'falar', 'estudar')
+
+
+class Aranha(Animal):
+    @property
+    def capacidade(self):
+        return super().capacidade + ('fazer teia', 'andar pelas paredes')
+
+
+class HomemAranha(Homem, Aranha):
+    @property
+    def capacidade(self):
+        return super().capacidade + \
+            ('bater em bandidos', 'atirar teias entre prédios')
+
+
+if __name__ == '__main__':
+    jhon = Homem()
+    print(f'John: {jhon.capacidade}')
+
+    aranha = Aranha()
+    print(f'aranha: {aranha.capacidade}')
+
+    peter = HomemAranha()
+    print(f'Peter Parker: {peter.capacidade}')
